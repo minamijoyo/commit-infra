9a98e539e839dbc14eefb35466d87ad6540b670c
minamijoyo <minamijoyo@gmail.com>
2015-08-15 00:04:16 +0900

    Update mysql cookbook to 6.1.0

---
+++ site-cookbooks/commitm-mysql/metadata.rb
@@ -0,0 +1,8 @@
+name             'commitm-mysql'
+maintainer       'minamijoyo'
+maintainer_email 'minamijoyo@gmail.com'
+license          'MIT'
+description      'Customize mysql settings for commit-m'
+long_description 'Customize mysql settings for commit-m'
+version          '0.1.0'
+depends          'mysql', '~> 6.1.0'
